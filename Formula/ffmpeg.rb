17092968bce72057f3fb08809ab267bcdd4e0da0
Aster HO <hhrayn@gmail.com>
2022-10-23 01:14:29 +0900

    update

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -1,155 +1,143 @@
 class Ffmpeg < Formula
-    desc "Play, record, convert, and stream audio and video"
-    homepage "https://ffmpeg.org/"
-    url "https://ffmpeg.org/releases/ffmpeg-5.0.1.tar.xz"
-    sha256 "ef2efae259ce80a240de48ec85ecb062cecca26e4352ffb3fda562c21a93007b"
-    # None of these parts are used by default, you have to explicitly pass `--enable-gpl`
-    # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
-    license "GPL-2.0-or-later"
-    head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
-  
-    livecheck do
-      url "https://ffmpeg.org/download.html"
-      regex(/href=.*?ffmpeg[._-]v?(\d+(?:\.\d+)+)\.t/i)
-    end
-  
-    bottle do
-      rebuild 1
-      sha256 arm64_monterey: "766f68ee9b87731b8b2b9a67140af5b7a4178542c83c2aa4790b3ec4d66951db"
-      sha256 arm64_big_sur:  "76abad2fdf37c88c02e6ff3c7c5dc5131f2cb1261e62f41903f10f9b93312932"
-      sha256 monterey:       "7f44e9040938f7b74484ceb8dca79bd8f31d30844255f074ae84ca905ce3d745"
-      sha256 big_sur:        "ab8be194324b7326c384f1db87f1687aaf6424ca9d69cdffcd227a385c66fca3"
-      sha256 catalina:       "f371c60da0c9aea8d818d9523dbf770088baa4bd98f4afd5bdace00689f5e2db"
-      sha256 x86_64_linux:   "4f032e8c0d38e0c1259ee3881682ececad8d3e74d99a75c3694f4afc1b54b1c5"
-    end
-  
-    depends_on "nasm" => :build
-    depends_on "pkg-config" => :build
-    depends_on "aom"
-    depends_on "dav1d"
-    depends_on "fdk-aac"
-    depends_on "fontconfig"
-    depends_on "freetype"
-    depends_on "frei0r"
-    depends_on "gnutls"
-    depends_on "lame"
-    depends_on "libass"
-    depends_on "libbluray"
-    depends_on "librist"
-    depends_on "libsoxr"
-    depends_on "libvidstab"
-    depends_on "libvmaf"
-    depends_on "libvorbis"
-    depends_on "libvpx"
-    depends_on "opencore-amr"
-    depends_on "openjpeg"
-    depends_on "opus"
-    depends_on "rav1e"
-    depends_on "rubberband"
-    depends_on "sdl2"
-    depends_on "snappy"
-    depends_on "speex"
-    depends_on "srt"
-    depends_on "tesseract"
-    depends_on "theora"
-    depends_on "webp"
-    depends_on "x264"
-    depends_on "x265"
-    depends_on "xvid"
-    depends_on "xz"
-    depends_on "zeromq"
-    depends_on "zimg"
-  
-    uses_from_macos "bzip2"
-    uses_from_macos "libxml2"
-    uses_from_macos "zlib"
-  
-    on_linux do
-      depends_on "libxv"
-      depends_on "gcc" # because rubbernand is compiled with gcc
-    end
-  
-    fails_with gcc: "5"
-  
-    def install
-      args = %W[
-        --prefix=#{prefix}
-        --enable-shared
-        --enable-pthreads
-        --enable-version3
-        --cc=#{ENV.cc}
-        --host-cflags=#{ENV.cflags}
-        --host-ldflags=#{ENV.ldflags}
-        --enable-ffplay
-        --enable-gnutls
-        --enable-gpl
-        --enable-libaom
-        --enable-libbluray
-        --enable-libdav1d
-        --enable-libmp3lame
-        --enable-libopus
-        --enable-librav1e
-        --enable-librist
-        --enable-librubberband
-        --enable-libsnappy
-        --enable-libsrt
-        --enable-libtesseract
-        --enable-libtheora
-        --enable-libvidstab
-        --enable-libvmaf
-        --enable-libvorbis
-        --enable-libvpx
-        --enable-libwebp
-        --enable-libx264
-        --enable-libx265
-        --enable-libxml2
-        --enable-libxvid
-        --enable-lzma
-        --enable-libfontconfig
-        --enable-libfreetype
-        --enable-frei0r
-        --enable-libass
-        --enable-libopencore-amrnb
-        --enable-libopencore-amrwb
-        --enable-libopenjpeg
-        --enable-libspeex
-        --enable-libsoxr
-        --enable-libzmq
-        --enable-libzimg
-        --enable-libfdk-aac
-        --enable-nonfree
-        --disable-libjack
-        --disable-indev=jack
-      ]
-  
-      # Needs corefoundation, coremedia, corevideo
-      args << "--enable-videotoolbox" if OS.mac?
-      args << "--enable-neon" if Hardware::CPU.arm?
-  
-      # Replace hardcoded default VMAF model path
-      unless build.head?
-        %w[doc/filters.texi libavfilter/vf_libvmaf.c].each do |f|
-          inreplace f, "/usr/local/share/model", HOMEBREW_PREFIX/"share/libvmaf/model"
-          # Since libvmaf v2.0.0, `.pkl` model files have been deprecated in favor of `.json` model files.
-          inreplace f, "vmaf_v0.6.1.pkl", "vmaf_v0.6.1.json"
-        end
-      end
-  
-      system "./configure", *args
-      system "make", "install"
-  
-      # Build and install additional FFmpeg tools
-      system "make", "alltools"
-      bin.install Dir["tools/*"].select { |f| File.executable? f }
-  
-      # Fix for Non-executables that were installed to bin/
-      mv bin/"python", pkgshare/"python", force: true
-    end
-  
-    test do
-      # Create an example mp4 file
-      mp4out = testpath/"video.mp4"
-      system bin/"ffmpeg", "-filter_complex", "testsrc=rate=1:duration=1", mp4out
-      assert_predicate mp4out, :exist?
-    end
+  desc "Play, record, convert, and stream audio and video"
+  homepage "https://ffmpeg.org/"
+  url "https://ffmpeg.org/releases/ffmpeg-5.1.2.tar.xz"
+  sha256 "619e706d662c8420859832ddc259cd4d4096a48a2ce1eefd052db9e440eef3dc"
+  # None of these parts are used by default, you have to explicitly pass `--enable-gpl`
+  # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
+  license "GPL-2.0-or-later"
+  head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
+
+  livecheck do
+    url "https://ffmpeg.org/download.html"
+    regex(/href=.*?ffmpeg[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
+
+  bottle do
+    sha256 arm64_monterey: "68649bcac737113215191cf50a564c3fe3ed63648cdad397677095ebdecee2cf"
+    sha256 arm64_big_sur:  "11b544745ec71743bf524ccc23c32413c982462bc34f81b78cbf931eaaf005bf"
+    sha256 monterey:       "79dc05514d77053ced330da6826bb8b6fb9b447b891587844e586b968b0fa451"
+    sha256 big_sur:        "2ef35dd30b1687b0abc1443bcfb8d9b661391fb2ef43f78003c0c26f299f54ae"
+    sha256 catalina:       "28e56206623d22a4bc589793f0462a9d95ad021fc1567a1eb9c5357f394615ab"
+    sha256 x86_64_linux:   "ea00042bb37e8657093e10b9798d0ccacd02c0660069d301d9e61a92d505a682"
+  end
+
+  depends_on "nasm" => :build
+  depends_on "pkg-config" => :build
+  depends_on "aom"
+  depends_on "dav1d"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "frei0r"
+  depends_on "gnutls"
+  depends_on "lame"
+  depends_on "libass"
+  depends_on "libbluray"
+  depends_on "librist"
+  depends_on "libsoxr"
+  depends_on "libvidstab"
+  depends_on "libvmaf"
+  depends_on "libvorbis"
+  depends_on "libvpx"
+  depends_on "opencore-amr"
+  depends_on "openjpeg"
+  depends_on "opus"
+  depends_on "rav1e"
+  depends_on "rubberband"
+  depends_on "sdl2"
+  depends_on "snappy"
+  depends_on "speex"
+  depends_on "srt"
+  depends_on "tesseract"
+  depends_on "theora"
+  depends_on "webp"
+  depends_on "x264"
+  depends_on "x265"
+  depends_on "xvid"
+  depends_on "xz"
+  depends_on "zeromq"
+  depends_on "zimg"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "alsa-lib"
+    depends_on "libxv"
+  end
+
+  fails_with gcc: "5"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --enable-shared
+      --enable-pthreads
+      --enable-version3
+      --cc=#{ENV.cc}
+      --host-cflags=#{ENV.cflags}
+      --host-ldflags=#{ENV.ldflags}
+      --enable-ffplay
+      --enable-gnutls
+      --enable-gpl
+      --enable-libaom
+      --enable-libbluray
+      --enable-libdav1d
+      --enable-libmp3lame
+      --enable-libopus
+      --enable-librav1e
+      --enable-librist
+      --enable-librubberband
+      --enable-libsnappy
+      --enable-libsrt
+      --enable-libtesseract
+      --enable-libtheora
+      --enable-libvidstab
+      --enable-libvmaf
+      --enable-libvorbis
+      --enable-libvpx
+      --enable-libwebp
+      --enable-libx264
+      --enable-libx265
+      --enable-libxml2
+      --enable-libxvid
+      --enable-lzma
+      --enable-libfontconfig
+      --enable-libfreetype
+      --enable-frei0r
+      --enable-libass
+      --enable-libopencore-amrnb
+      --enable-libopencore-amrwb
+      --enable-libopenjpeg
+      --enable-libspeex
+      --enable-libsoxr
+      --enable-libzmq
+      --enable-fdk-aac
+      --enable-libzimg
+      --disable-libjack
+      --disable-indev=jack
+    ]
+
+    # Needs corefoundation, coremedia, corevideo
+    args << "--enable-videotoolbox" if OS.mac?
+    args << "--enable-neon" if Hardware::CPU.arm?
+
+    system "./configure", *args
+    system "make", "install"
+
+    # Build and install additional FFmpeg tools
+    system "make", "alltools"
+    bin.install Dir["tools/*"].select { |f| File.executable? f }
+
+    # Fix for Non-executables that were installed to bin/
+    mv bin/"python", pkgshare/"python", force: true
+  end
+
+  test do
+    # Create an example mp4 file
+    mp4out = testpath/"video.mp4"
+    system bin/"ffmpeg", "-filter_complex", "testsrc=rate=1:duration=1", mp4out
+    assert_predicate mp4out, :exist?
+  end
+end
